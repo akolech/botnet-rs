06f15c7eac2215e6584db52e60322c71cb858974
akolech <kolechkoandrii@gmail.com>
2023-09-26 23:27:23 +0300

    request mock

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
-use tokio::time::{sleep, Duration};
+use rand::prelude::*;
+use tokio::time::{sleep, Duration, Instant};
+
+type MakeRequestResult = Result<String, String>;
+
+async fn make_request() -> MakeRequestResult {
+    let t: u64 = rand::thread_rng().gen_range(3..9);
+    sleep(Duration::from_secs(t)).await;
+    if t % 2 == 0 {
+        Ok("ok".to_string())
+    } else {
+        Err("err".to_string())
+    }
+}
+
+#[derive(Debug)]
+struct MakeRequestMeasures {
+    result: Result<String, String>,
+    delta: u64,
+}
+
+async fn make_request_measured() -> MakeRequestMeasures {
+    let before = Instant::now();
+    let result = make_request().await;
+    let after = Instant::now();
+    let delta = (after - before).as_secs();
+    MakeRequestMeasures { result, delta }
+}
 
 struct Monkey {
     sequence_number: u32,
@@ -10,9 +37,10 @@ impl Monkey {
 }
 
 async fn make_monkey_work(monkey: Monkey) {
+    let sequence_number = monkey.sequence_number;
     loop {
-        println!("Monkey's sequence number is {}", monkey.sequence_number);
-        sleep(Duration::from_secs(8)).await;
+        let report = make_request_measured().await;
+        println!("Monkey sequence number {sequence_number} report: {report:?}",)
     }
 }
 
@@ -21,7 +49,6 @@ async fn main() {
     for i in 0..8 {
         let monkey = Monkey::new(i);
         let _ = tokio::spawn(async move { make_monkey_work(monkey).await });
-        sleep(Duration::from_secs(1)).await;
     }
 
     loop {
