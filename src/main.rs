57eb55be650cd4e63f921bd51d38ead0fc3d29dc
akolech <kolechkoandrii@gmail.com>
2023-09-27 21:31:44 +0300

    overloaded connection simulation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use rand::prelude::*;
-use tokio::time::{sleep, Duration, Instant};
-use tokio::sync::{mpsc, oneshot};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::AtomicU8;
+use tokio::sync::{mpsc, oneshot};
+use tokio::time::{sleep, Duration, Instant};
 
 type MakeRequestResult = bool;
 
@@ -23,7 +23,7 @@ impl Monkey {
 
 async fn generate_resouces(pool: Arc<Mutex<u8>>) {
     loop {
-        if let Ok(mut  value) = pool.lock() {
+        if let Ok(mut value) = pool.lock() {
             *value = value.saturating_add(1);
         }
         sleep(Duration::from_millis(100)).await;
@@ -36,7 +36,7 @@ async fn process_request(mut rx: mpsc::Receiver<oneshot::Sender<bool>>) {
     let _ = tokio::spawn(async move { generate_resouces(generator_pool).await });
     while let Some(tx) = rx.recv().await {
         let mut counter = 0;
-        if let Ok(mut  value) = pool.lock() {
+        if let Ok(mut value) = pool.lock() {
             counter = value.saturating_sub(1);
             *value = counter
         }
@@ -51,7 +51,9 @@ async fn make_request(mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>) -> MakeReque
     oneshot_rx.await.unwrap()
 }
 
-async fn make_request_measured(mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>) -> MakeRequestMeasures {
+async fn make_request_measured(
+    mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>,
+) -> MakeRequestMeasures {
     let before = Instant::now();
     let result = make_request(mpsc_tx).await;
     let after = Instant::now();
@@ -59,29 +61,55 @@ async fn make_request_measured(mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>) -> 
     MakeRequestMeasures { result, delta }
 }
 
-async fn make_monkey_work(monkey: Monkey, mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>) {
+async fn make_monkey_work(
+    monkey: Monkey,
+    mpsc_tx: mpsc::Sender<oneshot::Sender<bool>>,
+    failing: &AtomicBool,
+) {
     let sequence_number = monkey.sequence_number;
     loop {
         sleep(Duration::from_secs(1)).await;
         let report = make_request_measured(mpsc_tx.clone()).await;
+        if !report.result {
+            failing.store(true, Ordering::Relaxed);
+        }
         println!("Monkey sequence number {sequence_number} report: {report:?}",)
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let (tx, rx) = mpsc::channel::<oneshot::Sender::<bool>>(32);
-
+    let (tx, rx) = mpsc::channel::<oneshot::Sender<bool>>(32);
     let _ = tokio::spawn(async move { process_request(rx).await });
+    let mut handles = vec![];
 
-    for i in 0..12 {
-        let monkey = Monkey::new(i);
-        let mpsc_tx = tx.clone();
-        let _ = tokio::spawn(async move { make_monkey_work(monkey, mpsc_tx).await });
-        sleep(Duration::from_millis(100)).await;
-    }
+    let failing = Arc::new(AtomicBool::new(false));
+    let mut stop = false;
 
     loop {
-        sleep(Duration::from_secs(60)).await;
+        let monkey = Monkey::new(42);
+        let mpsc_tx = tx.clone();
+        let failing_arc = Arc::clone(&failing);
+
+        if !stop {
+            handles.push(tokio::spawn(async move {
+                make_monkey_work(monkey, mpsc_tx, &failing_arc).await
+            }));
+        }
+
+        if failing.load(Ordering::Acquire) {
+            stop = true;
+            let handle = handles.pop().unwrap();
+            handle.abort();
+            failing.store(false, Ordering::Relaxed);
+        }
+
+        sleep(Duration::from_secs(1)).await;
+
+        println!("handles_count = {}", handles.len());
     }
+
+    // loop {
+    //     sleep(Duration::from_secs(60)).await;
+    // }
 }
